5ffcfc16c48869f4401ec65968b67274b2ef230f
asm128 <pabloarielz@outlook.com>
2019-09-05 02:23:03 -0300

    + Largely improved domainer SMTP add test. + Added preliminary code for SDomainer's block load.

--- test_unblocker/main.cpp
+++ test_unblocker/main.cpp
@@ -1,7 +1,7 @@
 #include "ubk_domainer.h"
 
 int main() {
-	::ubk::SSMTPMapBlock			smtpOriginal;
+	::ubk::SSMTPMapBlock			smtpOrigin;
 	const ::gpk::view_const_string	strings	[]	=
 		{"prueba0@prueba.com"
 		,"prueba1@prueba.com"
@@ -10,20 +10,29 @@ int main() {
 		};
 	::gpk::array_pod<char_t>		file;
 	for(uint32_t iString = 0; iString < ::gpk::size(strings); ++iString) {
-		const int32_t indexOfMap	= smtpOriginal.AddSMTPMap(strings[iString]);
+		const int32_t indexOfMap	= smtpOrigin.AddSMTPMap(strings[iString]);
 		ree_if((uint32_t)indexOfMap != iString, "%s", "Test failed!");
 		::gpk::array_pod<char_t>		stored;
-		smtpOriginal.GetEMail(indexOfMap, stored);
+		smtpOrigin.GetEMail(indexOfMap, stored);
 		ree_if(stored != strings[iString], "%s", "Test failed!");
 	}
-	smtpOriginal.Save(file);
+	smtpOrigin.Save(file);
 
 	::ubk::SSMTPMapBlock			smtpCopy;
 	smtpCopy.Load(file);
 	for(uint32_t iDomain = 0; iDomain < smtpCopy.Domain.size(); ++iDomain) {
-		ree_if(smtpCopy.Domain	[iDomain] != smtpOriginal.Domain	[iDomain], "%s", "Test failed!");
-		ree_if(smtpCopy.Username[iDomain] != smtpOriginal.Username	[iDomain], "%s", "Test failed!");
+		const uint32_t idxCopyDomain		= smtpCopy.Domain		[iDomain];
+		const uint32_t idxCopyUsername		= smtpCopy.Username		[iDomain];
+		const uint32_t idxOriginDomain		= smtpOrigin.Domain		[iDomain];
+		const uint32_t idxOriginUsername	= smtpOrigin.Username	[iDomain];
+		ree_if(idxCopyDomain	!= idxOriginDomain	, "%s", "Test failed!");
+		ree_if(idxCopyUsername	!= idxOriginUsername, "%s", "Test failed!");
+		const ::gpk::view_const_char	strOriginDomain		= {smtpOrigin	.Allocator.Views[idxOriginDomain	], smtpOrigin	.Allocator.Counts[idxOriginDomain	]};
+		const ::gpk::view_const_char	strOriginUsername	= {smtpOrigin	.Allocator.Views[idxOriginUsername	], smtpOrigin	.Allocator.Counts[idxOriginUsername	]};
+		const ::gpk::view_const_char	strCopyDomain		= {smtpCopy		.Allocator.Views[idxCopyDomain		], smtpCopy		.Allocator.Counts[idxCopyDomain		]};
+		const ::gpk::view_const_char	strCopyUsername		= {smtpCopy		.Allocator.Views[idxCopyUsername	], smtpCopy		.Allocator.Counts[idxCopyUsername	]};
+		ree_if(strCopyDomain	!= strOriginDomain		, "Origin: %s. Copy: %s.", ::gpk::toString(strOriginDomain	).begin(), ::gpk::toString(strCopyDomain	).begin());
+		ree_if(strCopyUsername	!= strOriginUsername	, "Origin: %s. Copy: %s.", ::gpk::toString(strOriginUsername).begin(), ::gpk::toString(strCopyUsername	).begin());
 	}
-
 	return 0;
 }
